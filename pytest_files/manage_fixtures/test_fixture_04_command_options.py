43694f5b0e9695272b457d7d93674b1a2dfb1431
nageswara <nageswara@nthallap.myguest.virtualbox.org>
2024-09-24 11:34:29 +0530

    deleted un necessory

--- pytest_files/manage_fixtures/test_fixture_04_command_options.py
+++
@@ -1,9 +0,0 @@
-
-
-def test_read_comd_option(cmd_opt_read):
-
-    print("\n", cmd_opt_read)
-
-def test_read_comd_option02(cmd_opt_read02):
-
-    print("\n", cmd_opt_read02)
--- pytest_files/manage_fixtures/test_fixtures02.py
+++
@@ -1,5 +0,0 @@
-
-
-def test_using_fixture(suite_fixture, sub_fixture):
-    print("suite fixture", suite_fixture)
-    print("sub fixture", sub_fixture)
